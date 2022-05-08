2023047d449cd7bacaa2ce5e39ba82d2820dc9e1
Azeezjim <azeexjim@gmail.com>
2022-05-07 22:20:46 -1100

    1st commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
+import { HiMenuAlt4, hiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import { images } from "../../constants";
 
 import "./Navbar.scss";
 
+const [toggle, setToggle] = useState(false)
 const Navbar = () => {
   return (
     <nav className="app__navbar">
@@ -14,12 +17,31 @@ const Navbar = () => {
         {["Home", "About", "Contact", "Work", "Skills", "Contacts"].map(
           (item) => (
             <li className="app__flex p-text" key={`link-${item}`}>
-              <div></div>
+              <div />
               <a href={`#${item}`}>{item}</a>
             </li>
           )
         )}
       </ul>
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{x: [300, 0]}}
+            transition= {{transition: 0.85, ease : 'easeOut' }}
+          >
+            <hiX onClick={() => setToggle(false)} />
+            {["Home", "About", "Contact", "Work", "Skills", "Contacts"].map(
+          (item) => (
+            <li key={item}>
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          )
+        )}
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
